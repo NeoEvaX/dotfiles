d97241d4f048b5b8575368a3b823047fde60422c
NeoEvaX <ian.kremer@gmail.com>
2024-06-28 00:01:39 -0700

    Moves Leader key to beginning

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
-require 'neoevax.config.keymaps'
-
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+require 'neoevax.config.keymaps'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
