563317c99fa4397030fe83da9cefce506b796617
NeoEvaX <ian.kremer@gmail.com>
2024-07-30 12:16:20 -0700

    Fixed some keymap bugs

--- .config/nvim/lua/neoevax/plugins/debug.lua
+++ .config/nvim/lua/neoevax/plugins/debug.lua
@@ -82,6 +82,15 @@ return {
       },
     }
 
+    -- Basic debugging keymaps, feel free to change to your liking!
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
+      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    end, { desc = 'Debug: Set Breakpoint' })
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
--- .config/nvim/lua/neoevax/plugins/harpoon.lua
+++ .config/nvim/lua/neoevax/plugins/harpoon.lua
@@ -6,9 +6,43 @@ return {
     config = function()
       local harpoon = require 'harpoon'
 
+      local keymap = vim.keymap -- for conciseness
       -- REQUIRED
       harpoon:setup()
       -- REQUIRED
+
+      -- Harpoon Keymaps
+      keymap.set('n', '<leader>ha', function()
+        harpoon:list():add()
+      end, { desc = '[A]dd file to Harpoon' })
+      keymap.set('n', '<leader>he', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Toggle Harpoon' })
+
+      keymap.set('n', '<C-1>', function()
+        harpoon:list():select(1)
+      end)
+      keymap.set('n', '<C-2>', function()
+        harpoon:list():select(2)
+      end)
+      keymap.set('n', '<C-3>', function()
+        harpoon:list():select(3)
+      end)
+      keymap.set('n', '<C-4>', function()
+        harpoon:list():select(4)
+      end)
+      keymap.set('n', '<leader>h1', function()
+        harpoon:list():select(1)
+      end)
+      keymap.set('n', '<leader>h2', function()
+        harpoon:list():select(2)
+      end)
+      keymap.set('n', '<leader>h3', function()
+        harpoon:list():select(3)
+      end)
+      keymap.set('n', '<leader>h4', function()
+        harpoon:list():select(4)
+      end)
     end,
   },
 }
