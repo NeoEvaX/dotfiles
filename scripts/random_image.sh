6ad4a98cf5647900e9c17e0e3d6cbe2b356b2418
NeoEvaX <ian.kremer@gmail.com>
2024-08-02 11:45:59 -0700

    Updated Random_image script

--- scripts/random_image.sh
+++ scripts/random_image.sh
@@ -2,9 +2,9 @@
 
 # Goes in the /usr/local/bin folder
 # Directory containing the images
-IMG_DIR=~/.warp/themes/SteamyBackgrounds
+IMG_DIR=~/.warp/themes/HomeBackgrounds
 
 # Get a random image from the directory
-random_image=$(find ~/.warp/themes/SteamyBackgrounds -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.JPEG" \) | /opt/homebrew/bin/gshuf -n 1)
+random_image=$(find ~/.warp/themes/HomeBackgrounds -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.JPEG" \) | /opt/homebrew/bin/gshuf -n 1)
 
 cp ${random_image} "${random_image%/*}/1.jpg" 
